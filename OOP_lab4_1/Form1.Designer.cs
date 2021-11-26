e2245a8f008a638d5c83c52d3fce58d2a156ea26
user name <grasstv14@gmail.com>
2021-11-27 00:18:09 +0500

    final

--- OOP_lab4_1/Form1.Designer.cs
+++ OOP_lab4_1/Form1.Designer.cs
@@ -29,48 +29,40 @@ namespace OOP_lab4_1
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // pictureBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(417, 183);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(121, 333);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.pictureBox1.BackColor = System.Drawing.Color.Snow;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(1768, 829);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(89, 90);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1880, 953);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- OOP_lab4_1/Form1.cs
+++ OOP_lab4_1/Form1.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Dynamic;
+using Rectangle = System.Drawing.Rectangle;
 
- namespace OOP_lab4_1
+
+
+namespace OOP_lab4_1
 {
-   
-
 
     public partial class Form1 : Form
     {
@@ -21,96 +15,283 @@ using System.Dynamic;
             InitializeComponent();
         }
 
+
+        Bitmap bmp = new Bitmap(1800, 800);
+        MyStorage str = new MyStorage();
+
+        public Bitmap Image { get; internal set; }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-           
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e) // переменная - счетчик, сколько сейчас есть объектов на форме, при del обнуляется, для раскрашивания круга в зеленый
+        {
+
         }
 
 
 
-
-
-        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
         {
-            int X = Cursor.Position.X;
-            int Y = Cursor.Position.Y;
-            Storage.Add(X,Y);
-            int vlX = Storage.GetValX(0);
-            int vlY = Storage.GetValY(0);
-            label1.Text = vlX.ToString() + ", " + vlY.ToString();
-            label2.Text = Storage.GetSize().ToString();
+            Graphics g = Graphics.FromImage(bmp);
+            Form1 snd = (Form1)sender;
+            if (e.KeyData == Keys.Delete)
+            {
+                str.Del(snd, bmp, g);
+                str.DrawAll(snd, bmp, g);
+                this.Refresh();
+            }
+            if (e.KeyData == Keys.Space)
+            {
+                str.DrawAll2(snd, bmp, g);
+                this.Refresh();
+            }
+        }
+
+        private void Form1_MouseUp_1(object sender, MouseEventArgs e)
+        {
+
+            Graphics g = Graphics.FromImage(bmp);
+            Form1 pb = (Form1)sender;
+
+
+            if (Control.ModifierKeys == Keys.Control)
+            { 
+                if (str.isHit2(e.X, e.Y, pb, bmp, g))
+                this.Refresh();
+           
+            }
+            else
+            {
+                if (str.isHit(e.X, e.Y, pb, bmp, g))
+                    this.Refresh();
+                else
+                {
+
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        str.Add(new CCircle(e.X, e.Y), pb, bmp, g);
+                    }
+                    if (e.Button == MouseButtons.Right)
+                    {
+
+                    }
+                    this.Refresh();
+                }
+            }
+            
         }
     }
 
-    public  class CCircle
+
+    public class CCircle
     {
-        public static int x,y;
-       public CCircle(int x_, int y_)
+
+        public int x, y;
+        public bool isSelected = false;
+        public int rad = 75;
+        public CCircle(int x_, int y_)
         {
             x = x_;
             y = y_;
-        }
-        public int GetValueX()
-        {
-            return(x);
-        }
-        public int GetValueY()
-        {
-            return (y);
         }
         ~CCircle()
         {
 
         }
 
+        public void DrawCircleBlack(Form1 sender, Bitmap bmp, Graphics g)
+        {
+            Rectangle rect = new Rectangle(x - rad, y - rad, rad * 2, rad * 2);
+            Pen pen = new Pen(Color.Black, 5);
+            isSelected = false;
+            g.DrawEllipse(pen, rect);
+            sender.BackgroundImage = bmp;
+
+        }
+
+        public void DrawCircleGreen(Form1 sender, Bitmap bmp, Graphics g)
+        {
+            Rectangle rect = new Rectangle(x - rad, y - rad, rad * 2, rad * 2);
+            Pen pen = new Pen(Color.Green, 5);
+            isSelected = true;
+            g.DrawEllipse(pen, rect);
+            sender.BackgroundImage = bmp;
+        }
+
+        public void DelCircle(Form1 sender, Bitmap bmp, Graphics g)
+        {
+            Rectangle rect = new Rectangle(x - rad, y - rad, rad * 2, rad * 2);
+            Pen pen = new Pen(Color.White, 5);
+            isSelected = true;
+            g.DrawEllipse(pen, rect);
+            sender.BackgroundImage = bmp;
+        }
+
+
+        public bool isHit(int x_, int y_)
+        {
+            if (((x - rad) < x_) && (x + rad > x_) && ((y - rad - rad) < y_) && (y + rad > y_))
+            { 
+                return true;
+            } 
+            else
+                return false;
+        }
+
     }
 
-     public class Storage 
+    public class MyStorage
     {
-        static public int size;
-        static public  CCircle[]  objects;
-     
-        public Storage()
+        static public int size, asize = 0;
+        static public CCircle[] objects;
+        static public CCircle[] allobjects;
+        public MyStorage()
         {
-            objects = new CCircle [0];
-            size = 0;
+            objects = new CCircle[100];
+            allobjects = new CCircle[100];
         }
 
-        ~Storage()
+        ~MyStorage()
         {
 
         }
-        static public int GetSize()
+
+        public void Drawing(int index, Form1 sender, Bitmap bmp, Graphics g)
+        {
+            if (objects[index] != null)
+                objects[index].DrawCircleGreen(sender, bmp, g);
+            DrawAll(sender, bmp, g);
+
+        }
+
+        public void DrawAll(Form1 sender, Bitmap bmp, Graphics g)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (objects[i] != null)
+                    objects[i].DrawCircleBlack(sender, bmp, g);
+            }
+        }
+
+        public void DrawAll2(Form1 sender, Bitmap bmp, Graphics g)
+        {
+            for (int i = 0; i < asize; i++)
+            {
+                    allobjects[i].DrawCircleBlack(sender, bmp, g);
+            }
+            for (int i = 0;  i< asize; i++)
+            objects[i] = allobjects[i];
+            size = asize;
+        }
+
+        public void DrawAll3(Form1 sender, Bitmap bmp, Graphics g)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (objects[i] != null) { 
+                    if (objects[i].isSelected)
+                objects[i].DrawCircleGreen(sender, bmp, g);
+                else
+                    objects[i].DrawCircleBlack(sender, bmp, g);
+                }
+            }
+        }
+
+        public int GetSize()
         {
             return (size);
         }
-        static public int GetValX(int index)
+
+        public int GetASize()
         {
-            return (objects[index].GetValueX());
+            return (asize);
         }
-        static public int GetValY(int index)
+
+        public void Add(CCircle obj, Form1 sender, Bitmap bmp, Graphics g)
         {
-            return (objects[index].GetValueY());
+            objects[size] = obj;
+            allobjects[asize] = obj;
+            Drawing(size, sender, bmp, g);
+            size++;
+            asize++;
         }
-        static public void Add(int x_, int y_)
+
+        public bool isHit(int x, int y, Form1 sender, Bitmap bmp, Graphics g)
         {
-            CCircle[] objects2 = new CCircle[size + 1];
-            if (size != 0)
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < size; j++)
+                if (objects[i] != null)
+                    if (objects[i].isHit(x, y))
                 {
-                    objects2[j] = (objects[j]);
+                    DrawAll(sender, bmp, g);
+                    objects[i].DrawCircleGreen(sender, bmp, g);
+                    return true;
                 }
             }
-            objects = null;
+            return false;
+        }
+
+        public bool isHit2(int x, int y, Form1 sender, Bitmap bmp, Graphics g)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (objects[i] != null)
+                if (objects[i].isHit(x, y))
+                {
+                    
+                    objects[i].DrawCircleGreen(sender, bmp, g);
+                    DrawAll3(sender, bmp, g);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /*
+        public void DelizMas(int j)
+        {
+            CCircle[] objects2 = new CCircle[100]; 
+
+            for ( int i = 0; i <j; i ++)
+            {
+                
+                objects2[i] = objects[i]; 
+                
+            }
+            for (int k = j+1; k<size; k++)
+            {
+                objects2[k-1] = objects[k];
+            }
             objects = objects2;
-            objects[size] = new CCircle(x_,y_);
-            size = size + 1;
-            
+            size--;
+
+
+        }
+        */
+
+        public void DelizMas(int j)
+        {
+            objects[j] = null;
+         
 
 
         }
 
+        public void Del(Form1 sender, Bitmap bmp, Graphics g)
+        {
+         for (int i = 0; i < size; i++)
+            {
+                if (objects[i] != null)
+                    if (objects[i].isSelected)
+                {
+                        objects[i].DelCircle(sender, bmp, g);
+                        DelizMas(i);
+                }
+            }
+        }
     }
 
 }
