5b33b68ad015962a982729588e6119092ea3e8c9
user name <grasstv14@gmail.com>
2021-11-29 01:22:49 +0500

    1

--- OOP_lab4_1/Form1.Designer.cs
+++ OOP_lab4_1/Form1.Designer.cs
@@ -29,31 +29,115 @@ namespace OOP_lab4_1
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(21, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DFS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.PeachPuff;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(53, 34);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(189, 99);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(431, 29);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Del - удалить все выделенные круги";
+            this.label1.Size = new System.Drawing.Size(92, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Путь: ";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(21, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Эйлер";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.PeachPuff;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(581, 34);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(189, 184);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(365, 29);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "пробел - отрисовать все круги";
+            this.label2.Size = new System.Drawing.Size(92, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Путь: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(322, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 38);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(292, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Начальная вершина:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PeachPuff;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(188, 697);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(959, 244);
+            this.panel1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(843, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 49);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "restart";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(1665, 881);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(178, 57);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "full restart";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
@@ -61,22 +145,29 @@ namespace OOP_lab4_1
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1880, 953);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp_1);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- OOP_lab4_1/Form1.cs
+++ OOP_lab4_1/Form1.cs
@@ -14,8 +14,9 @@ namespace OOP_lab4_1
         {
             InitializeComponent();
         }
-
-
+        int start = 0;
+        static int n = 25;
+        static int[,] arr = new int[n, n];
         Bitmap bmp = new Bitmap(1800, 800);
         MyStorage str = new MyStorage();
 
@@ -26,28 +27,14 @@ namespace OOP_lab4_1
             this.WindowState = FormWindowState.Maximized;
         }
 
-        private void Form1_MouseUp(object sender, MouseEventArgs e) // переменная - счетчик, сколько сейчас есть объектов на форме, при del обнуляется, для раскрашивания круга в зеленый
+        private void Wait(double seconds)
         {
-
-        }
-
-        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
-        {
-            Graphics g = Graphics.FromImage(bmp);
-            Form1 snd = (Form1)sender;
-            if (e.KeyData == Keys.Delete)
+            int ticks = System.Environment.TickCount + (int)Math.Round(seconds * 1000.0);
+            while (System.Environment.TickCount < ticks)
             {
-                str.Del(snd, bmp, g);
-                str.DrawAll(snd, bmp, g);
-                this.Refresh();
-            }
-            if (e.KeyData == Keys.Space)
-            {
-                str.DrawAll2(snd, bmp, g);
-                this.Refresh();
+                Application.DoEvents();
             }
         }
-
         private void Form1_MouseUp_1(object sender, MouseEventArgs e)
         {
 
@@ -55,41 +42,253 @@ namespace OOP_lab4_1
             Form1 pb = (Form1)sender;
 
 
-            if (Control.ModifierKeys == Keys.Control)
-            { 
-                if (str.isHit2(e.X, e.Y, pb, bmp, g))
-                this.Refresh();
            
-            }
-            else
-            {
                 if (str.isHit(e.X, e.Y, pb, bmp, g))
                     this.Refresh();
                 else
                 {
 
-                    if (e.Button == MouseButtons.Left)
-                    {
                         str.Add(new CCircle(e.X, e.Y), pb, bmp, g);
-                    }
-                    if (e.Button == MouseButtons.Right)
-                    {
 
-                    }
                     this.Refresh();
                 }
-            }
             
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = Graphics.FromImage(bmp);
+            Form1 pb = sender as Form1;
+            str.GetArr(arr);
+            bool[] visited = new bool[n];
+            void DFS(int st)
+            {
+                int r;
+                label1.Text += (st + 1).ToString();
+                label1.Text += ", ";
+                str.DLG(st, pb, bmp, g);
+                str.ZalivkaGreen(st, pb, bmp, g);
+               
+                this.Refresh();
+                Wait(1.5);
+                visited[st] = true;
+
+                for (r = 0; r < n; r++)
+                    if ((arr[st, r] != 0) && (!visited[r]))
+                        DFS(r);
+            }
+            for (int i = 0; i < n; i++)
+                visited[i] = false;
+
+            DFS(start);
+        }
+
+
+        public class Node
+        {
+            public int inf;
+            public Node next;
+        }
+
+        public void push(ref Node st, int dat)
+        { // Загрузка числа в стек
+
+            Node el = new Node();
+            el.inf = dat;
+            el.next = st;
+            st = el;
+        }
+
+        public int pop(ref Node st)
+        { // Извлечение из стека
+
+            int value = st.inf;
+            Node temp = st;
+            st = st.next;
+            temp = null;
+
+            return value;
+        }
+
+        public int peek(Node st)
+        { // Получение числа без его извлечения
+            return st.inf;
+        }
+
+        public Node[] graph; // Массив списков смежности
+
+        public void add(ref Node list, int data)
+        {
+            if (list == null)
+            {
+                list = new Node();
+                list.inf = data;
+                list.next = null;
+                return;
+            }
+
+            Node temp = list;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            Node elem = new Node();
+            elem.inf = data;
+            elem.next = null;
+            temp.next = elem;
+        }
+
+        public void del(ref Node l, int key)
+        { // Удаление вершины key из списка
+
+            if (l.inf == key)
+            {
+                Node tmp = l;
+                l = l.next;
+                tmp = null;
+            }
+            else
+            {
+                Node tmp = l;
+                while (tmp != null)
+                {
+                    if (tmp.next != null) // есть следующая вершина
+                    {
+                        if (tmp.next.inf == key)
+                        { // и она искомая
+                            Node tmp2 = tmp.next;
+                            tmp.next = tmp.next.next;
+                            tmp2 = null;
+                        }
+                    }
+                    tmp = tmp.next;
+                }
+            }
+        }
+
+        public int eiler(Node[] gr, int num)
+        { // Определение эйлеровости графа
+
+            int count;
+            for (int i = 0; i < num; i++)
+            { //проходим все вершины
+
+                count = 0;
+                Node tmp = gr[i];
+
+                while (tmp != null)
+                { // считаем степень
+                    count++;
+                    tmp = tmp.next;
+                }
+                if (count % 2 == 1)
+                {
+                    return 0; // степень нечетная
+                }
+            }
+            return 1; // все степени четные
+        }
+      
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics g = Graphics.FromImage(bmp);
+            Form1 pb = sender as Form1;
+            graph = new Node[n];
+            for (int i = 0; i < n; i++)
+            {
+                graph[i] = null;
+            }
+            str.GetArr(arr);
+            for (int i = 0; i < n; i++) // заполняем массив списков
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (arr[i, j] != 0)
+                    {
+                        add(ref graph[i], j);
+                    }
+                }
+            }
+
+
+            int res = eiler(graph, n);
+            if (res == 1)
+            {
+             
+                Node S = null; // Стек для  пройденных вершин
+                int u;
+
+                push(ref S, start); //сохраняем ее в стек
+                while (S != null)
+                { //пока стек не пуст
+                    start = peek(S); // текущая вершина
+                    if (graph[start] == null)
+                    { // если нет инцидентных ребер
+                        start = pop(ref S);
+
+
+                        label2.Text += (start + 1).ToString() + "  ";
+                        str.DLG(start, pb, bmp, g);
+                        str.ZalivkaGreen(start, pb, bmp, g);
+                        this.Refresh();
+                        Wait(1.5);
+
+                        //Wait(1.5);
+                    }
+                    else
+                    {
+                        u = graph[start].inf;
+                        push(ref S, u); //проходим в следующую вершину
+                        del(ref graph[start], u);
+                        del(ref graph[u], start); //удаляем пройденное ребро
+                    }
+                }
+            }
+            else
+            {
+                label2.Text = "Граф не является эйлеровым.";
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            { 
+            start = Int32.Parse(textBox1.Text);
+            start--;
+            }
+            else 
+            start = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Graphics g = Graphics.FromImage(bmp);
+            Form1 pb = sender as Form1;
+            str.DrawAll(pb, bmp, g);
+            textBox1.Text = "";
+            label1.Text = "Путь: ";
+            label2.Text = "Путь: ";
+            this.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+
         }
     }
 
 
     public class CCircle
     {
-
-        public int x, y;
+       
+        public int x, y, num;
         public bool isSelected = false;
-        public int rad = 75;
+        public int rad = 30;
         public CCircle(int x_, int y_)
         {
             x = x_;
@@ -100,56 +299,106 @@ namespace OOP_lab4_1
 
         }
 
-        public void DrawCircleBlack(Form1 sender, Bitmap bmp, Graphics g)
+        public void DrawCircleBlack(int size, Form1 sender, Bitmap bmp, Graphics g)
         {
+            num = size+1;
             Rectangle rect = new Rectangle(x - rad, y - rad, rad * 2, rad * 2);
             Pen pen = new Pen(Color.Black, 5);
-            isSelected = false;
+            Font font = new Font("Arial", 35, FontStyle.Regular);
+            
+            isSelected = true;
             g.DrawEllipse(pen, rect);
+
+            g.DrawString((num).ToString(), font, Brushes.Black, x - 20, y - 20);
             sender.BackgroundImage = bmp;
+            
+        }
+
+        public void Zalivka(Form1 sender, Bitmap bmp, Graphics g)
+        {
+            Font font = new Font("Arial", 35, FontStyle.Regular);
+            SolidBrush brush = new SolidBrush(Color.White);
+            g.FillEllipse(brush, x - rad, y - rad, rad * 2, rad * 2);
+            g.DrawString((num).ToString(), font, Brushes.Black, x - 20, y - 20);
+           
+        }
+
+        public void ZalivkaGreen(Form1 sender, Bitmap bmp, Graphics g)
+        {
+            Font font = new Font("Arial", 35, FontStyle.Regular);
+            SolidBrush brush = new SolidBrush(Color.Green);
+            g.FillEllipse(brush, x - rad, y - rad, rad * 2, rad * 2);
+            g.DrawString((num).ToString(), font, Brushes.Black, x - 20, y - 20);
 
         }
 
-        public void DrawCircleGreen(Form1 sender, Bitmap bmp, Graphics g)
+        public void DrawCircleGreen(int size, Form1 sender, Bitmap bmp, Graphics g)
         {
             Rectangle rect = new Rectangle(x - rad, y - rad, rad * 2, rad * 2);
             Pen pen = new Pen(Color.Green, 5);
+            Font font = new Font("Arial", 25, FontStyle.Regular);
             isSelected = true;
             g.DrawEllipse(pen, rect);
+            g.DrawString((size + 1).ToString(), font, Brushes.Green, x-20, y-20);
             sender.BackgroundImage = bmp;
         }
 
-        public void DelCircle(Form1 sender, Bitmap bmp, Graphics g)
+        public void DrawLine(int x1, int y1, int x2, int y2, Form1 sender, Bitmap bmp, Graphics g)
         {
-            Rectangle rect = new Rectangle(x - rad, y - rad, rad * 2, rad * 2);
-            Pen pen = new Pen(Color.White, 5);
-            isSelected = true;
-            g.DrawEllipse(pen, rect);
-            sender.BackgroundImage = bmp;
+            Pen p = new Pen(Color.Black, 7);
+            Point p1 = new Point(x1, y1);
+            Point p2 = new Point(x2, y2);
+            g.DrawLine(p, p1, p2);
+
         }
 
+        public void DrawLineGreen(int x1, int y1, int x2, int y2, Form1 sender, Bitmap bmp, Graphics g)
+        {
+            Pen p = new Pen(Color.Green, 7);
+            Point p1 = new Point(x1, y1);
+            Point p2 = new Point(x2, y2);
+            g.DrawLine(p, p1, p2);
 
+        }
+        
         public bool isHit(int x_, int y_)
         {
             if (((x - rad) < x_) && (x + rad > x_) && ((y - rad - rad) < y_) && (y + rad > y_))
             { 
+
                 return true;
             } 
             else
                 return false;
         }
 
+        public int GetCoorX()
+        {
+            return (x);
+        }
+        public int GetCoorY()
+        {
+            return (y);
+        }
     }
 
     public class MyStorage
     {
-        static public int size, asize = 0;
+        static int[,] arr2 = new int[25, 25];
+         
+        static public int size= 0;
+        static public int dlc = 0;
+        static public int x1, x2, y1, y2;
+        static public int  dl1  = -1;
+        static public int  dl2 = -1;
         static public CCircle[] objects;
-        static public CCircle[] allobjects;
+
         public MyStorage()
         {
             objects = new CCircle[100];
-            allobjects = new CCircle[100];
+            for (int i = 0; i < 25; i++)
+                for (int j = 0; j < 25; j++)
+                    arr2[i, j] = 0;
         }
 
         ~MyStorage()
@@ -160,105 +409,128 @@ namespace OOP_lab4_1
         public void Drawing(int index, Form1 sender, Bitmap bmp, Graphics g)
         {
             if (objects[index] != null)
-                objects[index].DrawCircleGreen(sender, bmp, g);
-            DrawAll(sender, bmp, g);
+                objects[index].DrawCircleBlack(size,sender, bmp, g);
 
         }
 
-        public void DrawAll(Form1 sender, Bitmap bmp, Graphics g)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                if (objects[i] != null)
-                    objects[i].DrawCircleBlack(sender, bmp, g);
-            }
-        }
-
-        public void DrawAll2(Form1 sender, Bitmap bmp, Graphics g)
-        {
-            for (int i = 0; i < asize; i++)
-            {
-                    allobjects[i].DrawCircleBlack(sender, bmp, g);
-            }
-            for (int i = 0;  i< asize; i++)
-            objects[i] = allobjects[i];
-            size = asize;
-        }
-
-        public void DrawAll3(Form1 sender, Bitmap bmp, Graphics g)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                if (objects[i] != null) { 
-                    if (objects[i].isSelected)
-                objects[i].DrawCircleGreen(sender, bmp, g);
-                else
-                    objects[i].DrawCircleBlack(sender, bmp, g);
-                }
-            }
-        }
 
         public int GetSize()
         {
             return (size);
         }
-
-        public int GetASize()
+        public void ZalivkaGreen(int index, Form1 sender, Bitmap bmp, Graphics g)
         {
-            return (asize);
+            objects[index].ZalivkaGreen(sender, bmp, g);
         }
-
+        public void GetArr(int[,] arr)
+        {
+            for (int i = 0; i < 25; i++)
+                for (int j = 0; j < 25; j++)
+                    arr[i, j] = arr2[i,j];
+        }
         public void Add(CCircle obj, Form1 sender, Bitmap bmp, Graphics g)
         {
             objects[size] = obj;
-            allobjects[asize] = obj;
+
             Drawing(size, sender, bmp, g);
             size++;
-            asize++;
-        }
 
+        }
+        public void DrawL(Form1 sender, Bitmap bmp, Graphics g)
+        {
+            x1 = objects[dl1].GetCoorX();
+            y1 = objects[dl1].GetCoorY();
+            x2 = objects[dl2].GetCoorX();
+            y2 = objects[dl2].GetCoorY();
+            objects[0].DrawLine(x1, y1, x2, y2, sender, bmp, g);
+            objects[dl1].Zalivka(sender, bmp, g);
+            objects[dl2].Zalivka(sender, bmp, g);
+            arr2[ dl1, dl2] = 1;
+            arr2[ dl2, dl1] = 1;
+        }
+        public void DrawLG(Form1 sender, Bitmap bmp, Graphics g)
+        {
+            x1 = objects[dl1].GetCoorX();
+            y1 = objects[dl1].GetCoorY();
+            x2 = objects[dl2].GetCoorX();
+            y2 = objects[dl2].GetCoorY();
+            objects[0].DrawLineGreen(x1, y1, x2, y2, sender, bmp, g);
+            objects[dl1].ZalivkaGreen(sender, bmp, g);
+            objects[dl2].ZalivkaGreen(sender, bmp, g);
+
+        }
+        public void DrawAll(Form1 sender, Bitmap bmp, Graphics g)
+        {
+            for (int i =0; i < size; i++ )
+            {
+                if (objects[i] != null)
+                    objects[i].Zalivka(sender, bmp, g);
+               for (int j =0; j<size; j++)
+                {
+                    if (arr2[i,j]==1)
+                    {
+                        dl1 = i;
+                        dl2 = j;
+                        DrawL(sender, bmp, g);
+                    }
+                }
+            }
+            dl1 = -1;
+            dl2 = -1;
+            dlc = 0;
+        }
+        public void DLG(int index, Form1 sender, Bitmap bmp, Graphics g)
+        {
+            if (dl1 == -1)
+            {
+                dl1 = index;
+                dlc++;
+            }
+            else
+            {
+                dl2 = index;
+                dlc++;
+            }
+            if (dlc == 2)
+            {
+                DrawLG(sender, bmp, g);
+                dl1 = dl2;
+                dl2 = -1;
+                dlc = 1;
+            }
+
+        }
         public bool isHit(int x, int y, Form1 sender, Bitmap bmp, Graphics g)
         {
             for (int i = 0; i < size; i++)
             {
                 if (objects[i] != null)
                     if (objects[i].isHit(x, y))
-                {
-                    DrawAll(sender, bmp, g);
-                    objects[i].DrawCircleGreen(sender, bmp, g);
+                    {
+                        if (dl1 == -1)
+                        { 
+                    dl1 = i;
+                    dlc++;
+                        }
+                        else
+                        {
+                            dl2 = i;
+                            dlc++;
+                        }
+                    if (dlc==2)
+                        {
+                            DrawL( sender,  bmp,  g);
+                            dl1 = -1;
+                            dl2 = -1;
+                            dlc = 0;
+                        }
                     return true;
-                }
+                    }
             }
             return false;
         }
 
-        public bool isHit2(int x, int y, Form1 sender, Bitmap bmp, Graphics g)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                if (objects[i] != null)
-                if (objects[i].isHit(x, y))
-                {
-                    objects[i].DrawCircleGreen(sender, bmp, g);
-                    DrawAll3(sender, bmp, g);
-                    return true;
-                }
-            }
-            return false;
-        }
 
-        public void Del(Form1 sender, Bitmap bmp, Graphics g)
-        {
-         for (int i = 0; i < size; i++)
-            {
-                if (objects[i] != null)
-                    if (objects[i].isSelected)
-                {
-                        objects[i].DelCircle(sender, bmp, g);
-                        objects[i] = null;
-                }
-            }
-        }
     }
 
 }
